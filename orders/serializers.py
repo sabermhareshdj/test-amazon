68bf1bda665e22a84eebd50d654711d84a73db37
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-10-02 15:08:55 +0300

    add get or create cart api

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,14 +1,17 @@
-from rest_framework.serializers import serializers                       
+from rest_framework import serializers                       
 from .models import Cart,CartDetail,Order,OrderDetail
+from product.serializers import ProductListSerializer
 
 class CartDetailSerializer(serializers.ModelSerializer):
+    #product = ProductListSerializer()
+    product = serializers.StringRelatedField()
     class Meta:
         model = CartDetail
         fields = '__all__'
 
 
 class CartSerializer(serializers.ModelSerializer):
-    CartDetail = CartDetailSerializer(many=True)
+    cart_Detail = CartDetailSerializer(many=True)
     class Meta:
         model = Cart
         fields = '__all__'
