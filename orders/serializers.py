b948d9591ff693757791d0e3782fa0bc60b809fa
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-10-02 15:59:02 +0300

    add cart update or create api

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,15 +3,15 @@ from .models import Cart,CartDetail,Order,OrderDetail
 from product.serializers import ProductListSerializer
 
 class CartDetailSerializer(serializers.ModelSerializer):
-    #product = ProductListSerializer()
-    product = serializers.StringRelatedField()
+    product = ProductListSerializer()
+    #product = serializers.StringRelatedField()
     class Meta:
         model = CartDetail
         fields = '__all__'
 
 
 class CartSerializer(serializers.ModelSerializer):
-    cart_Detail = CartDetailSerializer(many=True)
+    cart_detail = CartDetailSerializer(many=True)
     class Meta:
         model = Cart
         fields = '__all__'
