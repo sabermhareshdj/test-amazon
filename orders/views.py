99b2df78a356693f924a3c34a18dcacb54a5f454
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-10-02 13:25:31 +0300

    remove from cart

--- orders/views.py
+++ orders/views.py
@@ -29,6 +29,10 @@ def add_to_cart(request):
     return redirect(f'/products/{product.slug}')
 
 
+def remove_from_cart(request,id):
+    cart_detail = CartDetail.objects.get(id=id)
+    cart_detail.delete()
+    return redirect('/products/')
 
 
 @login_required
