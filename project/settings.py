2c8fd71b8cf9eb2e1bf6f35cebea1b39fe22d9d6
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-11-28 21:05:34 +0300

    add accounts p1

--- project/settings.py
+++ project/settings.py
@@ -124,22 +124,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-  #  'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mydatabase",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#DATABASES = {
+ #   "default": {
+  #      "ENGINE": "django.db.backends.postgresql",
+   #     "NAME": "mydatabase",
+    #    "USER": "postgres",
+     #   "PASSWORD": "postgres",
+      #  "HOST": "db",
+      #  "PORT": "5432",
+    #}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
