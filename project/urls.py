8460f75ca97c73d53dfd4d1e77b951f7eced1559
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-11-29 19:31:38 +0300

    add auth-views with login

--- project/urls.py
+++ project/urls.py
@@ -40,6 +40,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
     path('products/', include('product.urls',namespace='products')),
     path('orders/', include('orders.urls',namespace='orders')),
